fb4b6f48f983021e7b7957cd4326facb6a445ddd
kunalvirwal <149074679+kunalvirwal@users.noreply.github.com>
2024-03-22 18:37:50 +0530

    Update settings.py

--- Bracket/settings.py
+++ Bracket/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gjg*hu+bj+o6-qoqxvzgux63-u(t9%5!e0r6)jsvmxf*(hsq(&'
+SECRET_KEY = 'Enter your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
